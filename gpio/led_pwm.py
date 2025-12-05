a59940ca83454944a65bae5a68fc2a83ef03017c
Freddie Bullard <freddie.bullard@outlook.com>
2025-12-05 10:53:57 +0000

    Implemented LED class for PWM control

--- gpio/led_pwm.py
+++ gpio/led_pwm.py
@@ -2,19 +2,27 @@ from gpiozero import PWMLED
 from time import sleep
 
 class LED:
-    def __init__(self, pin: int):
+    def __init__(self, pin: int, val: float = 0.01):
         self.led = PWMLED(pin)
+        self.val = val
 
     def set_on(self, on: bool):
         if on:
             self.led.on()
         else:
             self.led.off()
+
+    def set_val(self, val: float):
+        self.led.value = val
+        self.val = val
+
+    def get_val(self):
+        return self.val
     
 
 if __name__ == '__main__':
     led_pin = 22
-    led = PWMLED(led_pin)
+    led = LED(led_pin)
     led.on()
 
     dv = 0.1
@@ -22,13 +30,13 @@ if __name__ == '__main__':
     val = 1
     while val >= 0:
         print(val)
-        led.value = val
+        led.set_val(val)
         val -= dv 
         sleep(0.1)
 
     while True:
         val = float(input('Enter value: ').strip())
         print(f'Setting val to {val}')
-        led.value = val
+        led.set_val(val)
 
 
