41bede20005ebd63aacdab27209c36ef80cb1d85
Freddie Bullard <freddie.bullard@outlook.com>
2026-02-03 15:53:43 +0000

    typo v3

--- src/capture.h
+++ src/capture.h
@@ -16,9 +16,9 @@ void close_device(xdtusb_device_t* handle);
 
 int get_frame_dims(xdtusb_device_t* handle, uint16_t* width, uint16_t* height);
 
-int read_register(xdtusb_device_t* handle, uint16_t* adr, uint16_t* val);
+int read_register(xdtusb_device_t* handle, uint16_t adr, uint16_t* val);
 
-int write_register(xdtusb_device_t* handle, uint16_t* adr, uint16_t val);
+int write_register(xdtusb_device_t* handle, uint16_t adr, uint16_t val);
 
 int capture_frame(
     xdtusb_device_t* handle,
