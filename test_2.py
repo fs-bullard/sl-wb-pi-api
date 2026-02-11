548c607cd4660641e911a39ebe0177211a1b3e7d
Freddie Bullard <freddie.bullard@outlook.com>
2026-02-11 16:08:51 +0000

    led test

--- test_2.py
+++ test_2.py
@@ -1,14 +1,17 @@
-from gpiozero import LED
+# LED pin 11
+# button pin 13
 
+from gpiozero import LED
+import RPI.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(13, GPIO.IN)
 
 led = LED(17)
-strip = LED(22)
-
 
 while True:
     if GPIO.input(13):
         led.on()
-        strip.off()
     else:
         led.off()
-        strip.on()
